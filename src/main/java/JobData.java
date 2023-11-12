8ef91712372ac2eb64d3ec1bf0595ad9271ad56e
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-11-12 13:43:03 -0600

    Task 2 Create Method findByValue

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -56,12 +56,12 @@ public class JobData {
     /**
      * Returns results of search the jobs data by key/value, using
      * inclusion of the search term.
-     *
+     * <p>
      * For example, searching for employer "Enterprise" will include results
      * with "Enterprise Holdings, Inc".
      *
-     * @param column   Column that should be searched.
-     * @param value Value of teh field to search for
+     * @param column Column that should be searched.
+     * @param value  Value of teh field to search for
      * @return List of all jobs matching the criteria
      */
     public static ArrayList<HashMap<String, String>> findByColumnAndValue(String column, String value) {
@@ -90,24 +90,31 @@ public class JobData {
      * @return List of all jobs with at least one field containing the value
      */
     public static ArrayList<HashMap<String, String>> findByValue(String value) {
-        ArrayList<HashMap<String, String>> matchingJobs = new ArrayList<>();
+
+        loadData();
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
 
         // Convert the search term to lowercase
-        String lowercaseValue = value.toLowerCase();
 
-        for (HashMap<String, String> job : allJobs) {
-            for (Map.Entry<String, String> field : job.entrySet()) {
-                // Convert field values to lowercase for case-insensitive comparison
-                String lowercaseFieldValue = field.getValue().toLowerCase();
-                if (lowercaseFieldValue.contains(lowercaseValue)) {
-                    matchingJobs.add(job);
-                    break;  // Exit the inner loop once a match is found
+        for (HashMap<String, String> row : allJobs) {
+
+            for (String key : row.keySet()) {
+                String aValue = row.get(key);
+
+                if (aValue.toLowerCase().contains(value.toLowerCase())) {
+                    jobs.add(row);
+
+                    break;
                 }
+                // Convert field values to lowercase for case-insensitive comparison
+
+                // Exit the inner loop once a match is found
             }
         }
 
-        return matchingJobs;
-    }
+        return jobs;
+}
 
     /**
      * Read in data from a CSV file and store it in a list
