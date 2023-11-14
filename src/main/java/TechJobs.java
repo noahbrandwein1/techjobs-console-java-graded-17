f399b3c1299bcfd661d10fbdd4d5cfa855e6819b
noah.brandwein1 <noah.brandwein98@gmail.com>
2023-11-14 00:22:13 -0600

    Task 3 case-insensitive

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -120,7 +120,7 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        if (someJobs.size() == 0) {
+        if (someJobs.isEmpty()) {
             System.out.print("No Results");
             return;
         }
